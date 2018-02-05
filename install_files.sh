7d4767885affcd74c3f3dc47449378d85c608343
Cade Brown <brown.cade@gmail.com>
2018-02-04 23:33:06 -0500

    samples and synthwave

--- install_files.sh
+++ install_files.sh
@@ -4,10 +4,9 @@
 
 LDIR="$HOME/lmms"
 
-mkdir $LDIR/plugins/vst
 
 cp -rf samples/* $LDIR/samples
 cp -rf plugins/* $LDIR/plugins
-cp -rf plugins/* $LDIR/plugins/vst
+cp -rf presets/* $LDIR/presets
 
 
