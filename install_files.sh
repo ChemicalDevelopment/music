182f695124c140006448a5390ecd8a80a7b32980
CadeBrown <brown.cade@gmail.com>
2018-04-20 08:33:06 -0400

    m

--- install_files.sh
+++ install_files.sh
@@ -5,9 +5,17 @@
 LDIR="$HOME/lmms"
 
 
-cp -rf samples/* $LDIR/samples
-cp -rf plugins/* $LDIR/plugins
-cp -rf presets/* $LDIR/presets
+if [ $# -eq 0 ]; then
+
+  cp -rf samples/* $LDIR/samples
+  cp -rf plugins/* $LDIR/plugins
+  cp -rf presets/* $LDIR/presets
+
+else
+ 
+  cp -rf $1 $LDIR/
+
+fi
 
 
 
